a9a21a420cfc73b7541563a433e74ea945b78810
Steve Witman <stevewitman@gmail.com>
2015-02-01 22:05:14 -0700

    added content security policy and new friends form

--- app/router.js
+++ app/router.js
@@ -6,7 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('friends', function() { });
+  this.resource('friends', function() {});
+  this.route('friends/new');
 });
 
 export default Router;
