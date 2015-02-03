00aa57864cce7baf2cce57f09d2e8a10e4471e20
Steve Witman <stevewitman@gmail.com>
2015-02-02 20:49:19 -0700

    added edit route

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -15,8 +15,15 @@ export default Ember.Controller.extend({
   ),
   actions: {
     save: function() {
-      console.log('+- save action in friends new controller');
-      return true;
+      if (this.get('isValid')) {
+        var _this = this;
+      this.get('model').save().then(function(friend) {
+      _this.transitionToRoute('friends.show', friend);
+    });
+    } else {
+      this.set('errorMessage', 'You have to fill all the fields');
+    }
+    return false;
     },
     cancel: function() {
       console.log('+- cancel action in friends new controller');
--- app/router.js
+++ app/router.js
@@ -6,8 +6,15 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('friends', function() { // see page 23
+  this.resource('friends', function() {
+    // see page 23 & 33
     this.route('new');
+    this.route('show', {
+      path: ":friend_id"
+    });
+    this.route("edit", {
+      path: ":friend_id/edit"
+    });
   });
 });
 
