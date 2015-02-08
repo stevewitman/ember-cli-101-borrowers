0c632f85399e7e07f5a1c02bd15f552e85067a2e
Steve Witman <stevewitman@gmail.com>
2015-02-07 23:25:23 -0700

    added route hooks to prevent unsaved records from displaying

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -1,6 +1,25 @@
-import Ember from 'ember';
+  import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('friend');
+  },
+  activate: function() {
+    console.log('----- activate hook called -----');
+  },
+  deactivate: function() {
+    // We grab the model loaded in this route
+    //
+    var model = this.modelFor('friends/new');
+    // If we are leaving the Route we verify if the model is in
+    // 'isNew' state, which means it wasn't saved to the backend.
+    //
+    if (model.get('isNew')) {
+    // We call DS#destroyRecord() which removes it from the store
+    //
+    model.destroyRecord();
+    }
+  },
   actions: {
     save: function() {
       console.log('+-- save action bubbled up to friends new route');
@@ -10,8 +29,5 @@ export default Ember.Route.extend({
       console.log('+-- cancel action bubbled up to friends new route');
       return true;
     }
-  },
-  model: function() {
-    return this.store.createRecord('friend');
   }
 });
